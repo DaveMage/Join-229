98d7223e4e4e8770b7ed8430822fd0a9d5d3be0a
Maik Wypior <160053500+MaikWpr83@users.noreply.github.com>
2024-06-02 11:14:02 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -34,3 +34,33 @@ function loadGuestLogin(){
 
     }
 }
+
+//Summary Content
+
+ /**
+   * This function switches the navbar on or out if the user clicks on the circle in the top corner.
+   */
+ function openNavbar() {
+    document.getElementById('navbar').classList.toggle('d-none');
+  }
+  
+  /**
+   * This function hides the navbar if the user clicks on the main container.
+   */
+  function closeNavbar() {
+    document.getElementById('navbar').classList.add('d-none');
+  }
+
+  /**functions goes to Board */
+function goToBoardUsual(mark) {
+    window.location.href = `./board.html#${mark}`;
+  }
+
+  /**
+ * function goes to the board and at the same time searches for the task
+ * which id is stored
+ */
+function goToBoard() {
+    window.location.href =
+      "./board.html?findtaskbyid=" + encodeURIComponent(holdTaskId);
+  }
