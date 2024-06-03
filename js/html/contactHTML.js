c4c16c99a8bfed5453660b527057d85a77b450f2
webstor21 <webstor.game@gmail.com>
2024-06-03 13:13:21 +0200

    Update contactHTML.js

--- js/html/contactHTML.js
+++ js/html/contactHTML.js
@@ -47,7 +47,7 @@ function contactListItemHtml(contact) {
 
 function contactViewHtml(contact) {
     return `
-    <section class="contactView" onclick="closeOption()">
+    <section class="contactView" onclick="closeOption()" id="contactViewContainer${contact.id}">
     <img class="arrowBack" src="/img/Mobile/Contacts/arrowLeftBlue.png" onclick="goToContacts()"/>
     <div class="ContactViewHeader">
       <span class="contactViewHeadline">Contacts</span>
@@ -56,15 +56,15 @@ function contactViewHtml(contact) {
     </div>
     <div class="contactViewBody">
       <div class="contactViewProfileIconName">
-        <div class="contactViewProfileIcon"
-        style="background-color: ${contact.profileColor};">${contact.initials}</div>
-        <div class="contactViewName">${contact.name}</div>
+        <div class="contactViewProfileIcon" id="contactViewProfileIcon"
+        style="background-color:${contact.profileColor};">${contact.initials}</div>
+        <div class="contactViewName" id="contactViewName">${contact.name}</div>
       </div>
       <span class="contactViewSubheadline">Contact Information</span>
       <div class="contactViewInfo">
         <div class="contactViewSubContainer">
           <span class="contactViewLabel">Email</span>
-          <a href="mailto:${contact.email}" class="contactViewLink" id="contactViewEmail">${contact.email}</a>
+          <a href="mailto:${contact.email}" target="_blank" class="contactViewLink" id="contactViewEmail">${contact.email}</a>
         </div>
         <div class="contactViewSubContainer">
           <span class="contactViewLabel">Phone</span>
@@ -84,7 +84,7 @@ function contactViewHtml(contact) {
 
 function optionHtml(contact){
     return `<div class="optionContainer" id="optionContainer">
-    <button><img src="/img/Mobile/Contacts/editGrey.png" class="editBtn">Edit</button>
+    <button onclick="openEditContact('${contact.id}')"><img src="/img/Mobile/Contacts/editGrey.png" class="editBtn">Edit</button>
     <button onclick="deleteContact('${contact.id}')"><img src="/img/Mobile/Contacts/trashGrey.png" class="deleteBtn" >Delete</button>
   </div>`;
 }
@@ -97,3 +97,40 @@ function successfullyHtml(){
     </div>
     </div>
     `;}
+
+  function contactEditForm(contact){
+    return`
+    <div class="background " id="contactEditFormBackground">
+    <div class="addContactContainer slideInBottom" id="editContactContainer">
+        <div class="addContainerTopContainer">
+            <img class="closeBtn" src="/img/Mobile/Contacts/closeWhite.png" onclick="closeEditContact()">
+            <h1 class="contactHeadline">Add Contact</h1>                
+            <span class="headlineUnderline"></span>
+        </div>            
+        <div class="addContactContainerBottom">
+            <form onsubmit="return false;">
+                <div class="contactProfileIcon" style="background-color: ${contact.profileColor};">
+                  ${contact.initials}
+                </div>
+                <label for="contactName${contact.id}" class="contactLabel">
+                    <input type="text" id="contactName${contact.id}" placeholder="Name" required value="${contact.name}">
+                    <img src="/img/Mobile/Contacts/personIconContacts.png">
+                </label>
+                <label for="contactEmail${contact.id}" class="contactLabel">
+                    <input type="email" id="contactEmail${contact.id}" placeholder="Email" value="${contact.email}">
+                    <img src="/img/Mobile/Contacts/mailIconContacts.png">
+                </label>
+                <label for="contactPhone${contact.id}" class="contactLabel">
+                    <input type="tel" id="contactPhone${contact.id}" placeholder="Phone" value="${contact.phone}">
+                    <img src="/img/Mobile/Contacts/callIconContacts.png" >
+                </label>
+                <div class="contactEditFormBtnContainer">
+                  <button class="secondaryBtn" onclick="deleteContact('${contact.id}')">Delete</button>
+                  <button onclick="saveEditContact('${contact.id}')" class="primaryBtn saveContactBtn">Save<img src="/img/Mobile/Contacts/checkWhite.png"></button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+    `;
+  }
