9c0ac936965c669e0094da124adb7012c1cd0a0b
webstor21 <webstor.game@gmail.com>
2024-06-04 16:00:52 +0200

    Update board.js

--- js/board.js
+++ js/board.js
@@ -12,3 +12,19 @@ function toggleAssignedDropdown() {
         }
     
 }
+
+function toggleCategoryDropdown() {
+    const dropdown = document.getElementById('dropdownCategory');    
+    let icon = document.getElementById('categoryDropdownArrow');   
+    
+        if (dropdown.style.display === 'flex') {            
+            dropdown.style.display = 'none';
+            icon.style.transform = 'rotate(0deg)';        
+        } else {
+            dropdown.style.display = 'flex';            
+            icon.style.transform = 'rotate(180deg)';
+            
+        }
+    
+}
+
