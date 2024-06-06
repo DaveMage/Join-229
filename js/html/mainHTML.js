dcecc26e553cc533555318c15bc211734f01b36f
webstor21 <webstor.game@gmail.com>
2024-06-06 19:56:20 +0200

    Update mainHTML.js

--- js/html/mainHTML.js
+++ js/html/mainHTML.js
@@ -13,14 +13,14 @@ function menuMobileHtml(){
    <a class="menuMobile" href="/summary.html">
        <img src="/img/Mobile/generalElements/summaryIconGray.png" alt="Summary Icon" id="summaryMobileIcon">
        <p>Summary</p>
+   </a>   
+   <a class="menuMobile" href="/board.html">
+       <img src="/img/Mobile/generalElements/boardIconGray.png" alt="Board Icon" id="boardMobileIcon">
+       <p>Board</p>
    </a>
    <a class="menuMobile" href="/addTask.html">
        <img src="/img/Mobile/generalElements/addTaskIconGray.png" alt="Add Task Icon" id="addTaskMobileIcon">
        <p>Add Task</p>
-   </a>
-   <a class="menuMobile" href="/board.html">
-       <img src="/img/Mobile/generalElements/boardIconGray.png" alt="Board Icon" id="boardMobileIcon">
-       <p>Board</p>
    </a>
    <a class="menuMobile" href="/contacts.html">
        <img src="/img/Mobile/generalElements/contactsIconGray.png" alt="Contacs Icon" id="contactsMobileIcon">
