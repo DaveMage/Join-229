37f27bf5ef6dc403e52f76b948fd554c63a5a1fd
webstor21 <webstor.game@gmail.com>
2024-06-06 19:56:22 +0200

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,5 +1,7 @@
 const BASE_URL = 'https://join-229-c9c59-default-rtdb.europe-west1.firebasedatabase.app';
 let contacts = [];
+let tasks = [];
+let users = []; 
 
 async function loadData(path = '') {
     let response = await fetch(BASE_URL + path + '.json');
@@ -37,6 +39,7 @@ async function putData(path = '', data = {}) {
 }
 
 let profileColor = ['#FF7A00', '#FF5EB3', '#6E52FF', '#9327FF', '#00BEE8', '#1FD7C1', '#FF745E', '#FFA35E', '#FC71FF', '#FFC701', '#0038FF', '#C3FF2B', '#FFE62B', '#FF4646', '#FFBB2B']
+
 
 
 
