44ce47799e3a11053ddb293d8ba06bbf7f07a77e
Rubi007007 <151785420+Rubi007007@users.noreply.github.com>
2024-06-07 19:49:26 +0200

    Task nun aufrufbar mit Animation
    Animation eingefügt (fehlt noch beim schließen der Task -> wie geht das..?), Hintergrund nicht scrollbar wenn die Task geöffnet ist, Bilder von den Pluszeichen ändern sich nun, sobald man draufklickt

--- js/board.js
+++ js/board.js
@@ -28,3 +28,20 @@ function toggleCategoryDropdown() {
     
 }
 
+function openTask() {
+    let task = document.getElementById('taskOverlay');
+    task.classList.remove('dNone');
+    let content = document.getElementById('wholeContent');
+    content.classList.add('overflowHidden');
+    let body = document.getElementById('template');
+    body.classList.add('overflowHidden');
+}
+
+function closeTask() {
+    let task = document.getElementById('taskOverlay');
+    task.classList.add('dNone');
+    let content = document.getElementById('wholeContent');
+    content.classList.remove('overflowHidden');
+    let body = document.getElementById('template');
+    body.classList.remove('overflowHidden');
+}
