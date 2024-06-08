efcc40110890958c3e3d439892995e9d1fd1eae2
webstor21 <webstor.game@gmail.com>
2024-06-08 03:50:14 +0200

    Update signUp.js

--- js/signUp.js
+++ js/signUp.js
@@ -16,3 +16,36 @@ function goToLogin() {
     window.location.href = "/login.html";
 }
 
+function signUp() {
+
+    let name = document.getElementById('signUpName').value;
+    let email = document.getElementById('signUpEmail').value;
+    let password = document.getElementById('signUpPassword').value;
+    let confirmPassword = document.getElementById('signUpConfirmPassword').value;
+    console.log(name, email, password, confirmPassword);
+
+    if (name === '' || email === '' || password === '' || confirmPassword === '') {
+        alert('Please fill in all fields');
+        return;// name nur als Fullname
+    }
+
+    if (password !== confirmPassword) {
+        alert('Passwords do not match');//auslagern und als onchange event setzen
+        return;
+    }
+
+    try {
+        postData('/users', {
+            'name': name,
+            'email': email,
+            'password': password
+        });
+
+        alert('Sign up successful');// animation einbauen
+    } catch (error) {
+        console.error('Error signing up:', error);
+    }
+
+
+
+}
