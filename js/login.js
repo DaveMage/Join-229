ea4bef6ccedccc306915b0d91a209bc7a29ac28d
webstor21 <webstor.game@gmail.com>
2024-06-09 14:19:53 +0200

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-function goToSighUp(){
+function goToSighUp() {
     window.location.href = "/signUp.html";
 }
 
@@ -15,8 +15,28 @@ function resetFocus() {
     });
 }
 
-function guestLogin(){
+function guestLogin() {
     window.location.href = "/summary.html";
     localStorage.setItem('guestLoggedIn', 'true');
 }
 
+
+
+/**
+ * Retrieves contacts from the server and displays them on the webpage.
+ * @returns {Array} An array of contact objects.
+ */
+async function getContacts() {
+    let response = await fetch(BASE_URL + '/users.json');
+    let responseToJson = await response.json();
+    let fetchedUsers = [];
+
+    for (const key in responseToJson) {
+        let user = responseToJson[key];
+        user.id = key;
+        fetchedUsers.push(user);
+    }
+
+    user = fetchedUsers; // Store fetched contacts in a global variable
+    return user;
+}
