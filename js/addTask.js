19a9a717443db87c745370ff3ea928b4aafe9a50
webstor21 <webstor.game@gmail.com>
2024-06-11 18:39:46 +0200

    Update addTask.js

--- js/addTask.js
+++ js/addTask.js
@@ -4,3 +4,17 @@ function dateTreshhold() {
     let today = new Date().toISOString().split('T')[0];
     document.getElementById("addTaskDueDate").setAttribute('min', today);
 };
+
+function saveTask(){
+    let title = document.getElementById("addTaskTitle");
+
+    if(title.value === ""){
+        title.classList.add('errorLabel');
+        document.getElementById('errorSpanTitle').style.display = 'block';        
+        return;
+    } else {
+        title.classList.remove('errorLabel');
+        document.getElementById('errorSpanTitle').style.display = 'none';
+        document.getElementById('errorSpanTitle').textContent = 'ne';
+    }
+}
