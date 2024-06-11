116a20d0917634c822cf21db3492cd55a4564bc6
webstor21 <webstor.game@gmail.com>
2024-06-11 15:19:28 +0200

    Update signUp.js

--- js/signUp.js
+++ js/signUp.js
@@ -166,21 +166,3 @@ async function createUser(user) {
         console.error('Error creating user:', error);
     }
 }
-
-
-function focusInput(inputId) {
-    const ids = ["signUpLabelName", "signUpLabelEmail", "signUpLabelPassword", "signUpLabelConfirmPassword"];
-    ids.forEach(id => {
-        document.getElementById(id).style.borderColor = id === inputId ? "#29ABE2" : "#D1D1D1";
-    });
-}
-function resetFocus() {
-    const ids = ["signUpLabelName", "signUpLabelEmail", "signUpLabelPassword", "signUpLabelConfirmPassword"];
-    ids.forEach(id => {
-        document.getElementById(id).style.borderColor = "#D1D1D1";
-    });
-}
-
-function goToLogin() {
-    window.location.href = "/login.html";
-}
