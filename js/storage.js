67da254ab49da454f3bbb1372b9ea2c528cd3b43
webstor21 <webstor.game@gmail.com>
2024-06-11 21:29:06 +0200

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -41,26 +41,73 @@ async function putData(path = '', data = {}) {
 let profileColor = ['#FF7A00', '#FF5EB3', '#6E52FF', '#9327FF', '#00BEE8', '#1FD7C1', '#FF745E', '#FFA35E', '#FC71FF', '#FFC701', '#0038FF', '#C3FF2B', '#FFE62B', '#FF4646', '#FFBB2B']
 
 
-/**
- * Retrieves users from the server and displays them on the webpage.
- * @returns {Array} An array of user objects.
- */
-async function getUsers() {
+let user = [];
+
+
+
+async function getUser(){
     let response = await fetch(BASE_URL + '/users.json');
     let responseToJson = await response.json();
-    let fetchedUsers = [];
+    let fetchedUser = [];
 
     for (const key in responseToJson) {
         let user = responseToJson[key];
         user.id = key;
-        fetchedUsers.push(user);
+        fetchedUser.push({id: user.id, email: user.email});
     }
 
-    users = fetchedUsers; // Store fetched contacts in a global variable
-    return users;
+    console.log(fetchedUser);
+    return fetchedUser;
+}
+ 
+function loginSuccess(user, rememberMe) {
+    if (rememberMe) {
+        localStorage.setItem('userEmail', user.email);
+        localStorage.setItem('userPassword', user.password);
+        localStorage.setItem('userId', user.id);
+    } else {
+        localStorage.setItem('userEmail', user.email);
+    }
+    localStorage.setItem('user', JSON.stringify(user));
+    
+    // Update the currentUser variable
+    currentUser.push(user.id);
+
+    // Redirect to the dashboard or another page after successful login
+    window.location.href = "/summary.html";
 }
 
+async function getUser(){
+    let response = await fetch(BASE_URL + '/users.json');
+    let responseToJson = await response.json();
+    let fetchedUser = [];
 
+    for (const key in responseToJson) {
+        let user = responseToJson[key];
+        user.id = key;
+        fetchedUser.push({id: user.id, email: user.email});
+    }
 
+    console.log(fetchedUser);
+    return fetchedUser;
+}
+
+async function getUserIdByEmail() {
+    let users = await getUser();
+    let userEmail = localStorage.getItem('userEmail');
+    let user = users.find(user => user.email === userEmail);
+    if (user) {
+        let userId = user.id;
+        console.log('User ID:', userId);
+        currentUser.push(userId);
+        console.log('Current User:', currentUser);
+        return userId;
+    } else {
+        console.log('User not found');
+        return null;
+    }
+}
+
+let currentUser = [];
 
 
