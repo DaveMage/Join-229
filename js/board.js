83fc7b53fcb202edebf0ee924b8b710543c02ba1
Rubi007007 <151785420+Rubi007007@users.noreply.github.com>
2024-06-12 20:04:39 +0200

    editTask Overlay - Version 2

--- js/board.js
+++ js/board.js
@@ -45,3 +45,19 @@ function closeTask() {
     let body = document.getElementById('template');
     body.classList.remove('overflowHidden');
 }
+
+function openEditTask() {
+    let task = document.getElementById('taskOverlay');
+    task.classList.add('dNone');
+    let editTask = document.getElementById('editTaskOverlay');
+    editTask.classList.remove('dNone');
+}
+
+function closeEditTask() {
+    let editTask = document.getElementById('editTaskOverlay');
+    editTask.classList.add('dNone');
+    let content = document.getElementById('wholeContent');
+    content.classList.remove('overflowHidden');
+    let body = document.getElementById('template');
+    body.classList.remove('overflowHidden');
+}
