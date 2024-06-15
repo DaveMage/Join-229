0d477b474128c8e0a808cb21e52f29774497c86c
Rubi007007 <151785420+Rubi007007@users.noreply.github.com>
2024-06-15 21:41:14 +0200

    editTask - dropdown

--- js/board.js
+++ js/board.js
@@ -1,4 +1,32 @@
+function toggleAssignedDropdown() {
+    const dropdown = document.getElementById('dropdownAssigned');    
+    let icon = document.getElementById('assignedDropdownArrow');   
+    
+        if (dropdown.style.display === 'flex') {            
+            dropdown.style.display = 'none';
+            icon.style.transform = 'rotate(0deg)';        
+        } else {
+            dropdown.style.display = 'flex';            
+            icon.style.transform = 'rotate(180deg)';
+            
+        }
+    
+}
 
+function toggleCategoryDropdown() {
+    const dropdown = document.getElementById('dropdownCategory');    
+    let icon = document.getElementById('categoryDropdownArrow');   
+    
+        if (dropdown.style.display === 'flex') {            
+            dropdown.style.display = 'none';
+            icon.style.transform = 'rotate(0deg)';        
+        } else {
+            dropdown.style.display = 'flex';            
+            icon.style.transform = 'rotate(180deg)';
+            
+        }
+    
+}
 
 function openTask() {
     let task = document.getElementById('taskOverlay');
