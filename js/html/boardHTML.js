8588100e29f3bf8adfb816f8bb583bcb6bbdd860
webstor21 <webstor.game@gmail.com>
2024-06-17 19:40:17 +0200

    Update boardHTML.js

--- js/html/boardHTML.js
+++ js/html/boardHTML.js
@@ -30,25 +30,21 @@ function loadEmptyDoneColumn() {
     `;
 }
 
-function loadTasksHTML() {
+function loadTasksHTML(task) {    
     return /*html*/ `
         <div class="taskCard" id="taskCard0" onclick="openTask()" draggable="true" ondragstart="dragTask()">
             <div class="cardBody">
-                <p class="userStory">User Story</p>
+                <p class="userStory">${task.Category}</p>
                 <div class="taskHeadline">
-                    Kochwelt Page & Recipe Recommender
+                    ${task.title}
                 </div>
                 <div class="taskDescr">
-                    Build start page with recipe recommendation...
+                    ${task.description}
                 </div>
-                <div class="subtasks">
-                    <div class="progressBar"></div>
-                    <div class="progressText">
-                        <span class="complTasks">2</span>/<span class="totalNumOfTasks">2</span>Subtasks</div>
-                </div>
+                    ${subtaskProgressbarHTML()}
                 <div class="taskFooter">
                     <div class="profiles">
-                        <div class="profileIcon" style="background-color:#FFBB2B;">AM</div>
+                        <div class="profileIcon" style="background-color:#FFBB2B;">${task.Assigned[0].initials}</div>
                         <div class="profileIcon" style="background-color:#FFA35E;">TM</div>
                         <div class="profileIcon" style="background-color:#1FD7C1;">DM</div>
                     </div>
@@ -56,30 +52,16 @@ function loadTasksHTML() {
                 </div>
             </div>
         </div>
-        <div class="taskCard" id="taskCard1" onclick="openTask()" draggable="true" ondragstart="dragTask()">
-            <div class="cardBody">
-                <p class="technicalTask">Technical Task</p>
-                <div class="taskHeadline">
-                    HTML Base Template Creation
-                </div>
-                <div class="taskDescr">
-                    Create reusable HTML base templates...
-                </div>
-                <div class="subtasks">
-                    <div class="progressBar"></div>
-                    <div class="progressText">
-                        <span class="complTasks">2</span>/<span class="totalNumOfTasks">2</span>Subtasks
-                    </div>
-                </div>
-                <div class="taskFooter">
-                    <div class="profiles">
-                        <div class="profileIcon" style="background-color:#FFBB2B;">AM</div>
-                        <div class="profileIcon" style="background-color:#FFA35E;">TM</div>
-                        <div class="profileIcon" style="background-color:#1FD7C1;">DM</div>
-                    </div>
-                    <img class="prioritySymbol" src="./img/Mobile/Board/priorityImportant.png" alt="">
-                </div>
-            </div>
-        </div>
+        
+    `;
+}
+
+function subtaskProgressbarHTML(){
+    return `
+    <div class="subtasks">
+    <div class="progressBar"></div>
+    <div class="progressText">
+        <span class="complTasks">2</span>/<span class="totalNumOfTasks">2</span>Subtasks</div>
+    </div>
     `;
 }
