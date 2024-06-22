7fb83a83dd504dc7638f7f82bc296104f7734b18
webstor21 <webstor.game@gmail.com>
2024-06-22 16:39:20 +0200

    Update boardHTML.js

--- js/html/boardHTML.js
+++ js/html/boardHTML.js
@@ -32,7 +32,7 @@ function loadEmptyDoneColumn() {
 
 function loadTasksHTML(task) {
     return /*html*/ `
-        <div class="taskCard" id="taskCard${task.id}" draggable="true" ondragstart="dragTask()" onclick="viewTask2('${task.id}')">
+        <div class="taskCard" id="taskCard${task.id}" draggable="true" ondragstart="dragTask()" onclick="openTask('${task.id}')">
             <div class="cardBody">
                 <p class="${((task.category == 'Technical Task') ? ('technicalTask') : (task.category == 'User Story') ? ('userStory') : (''))}">${task.category}</p>
                 <div class="taskCardHeadlineDescription">
@@ -90,56 +90,52 @@ function subtaskProgressbarHTML(task) {
     return subtaskContentHtml;
 }
 
-function viewTask2(task) {
-    return `
-        <div class="test">${task.id}</div>
-    `;
-}
 
 function viewTask(task) {
     return /*html*/ `
-        <div class="overlayTask" id="task${task.id}">
-            <div class="overlayHeader">
-                <p class="userStory userStoryOverlay">${task.category}</p>
-                <img onclick="closeTask()" class="closeTask" src="./img/Mobile/Board/closeTask.png" alt="close Task">
-            </div>
-            <h1 class="overlayHeadline">${task.title}</h1>
-            <p class="overlayDescription">
-                ${task.description}
-            </p>
-            <div class="taskBody">
-                <div class="dueDateSection bodySection">
-                    <span class="dueDate spanDatePriorityAssigned">Due date:</span>
-                    <span class="date">${task.date}</span>
+        <div class="taskOverlayBackground">
+            <div class="overlayTask" id="task${task.id}">
+                <div class="overlayHeader">
+                    <p class="userStory userStoryOverlay">${task.category}</p>
+                    <img onclick="closeTask()" class="closeTask" src="./img/Mobile/Board/closeTask.png" alt="close Task">
                 </div>
-                <div class="prioritySection bodySection">
-                    <span class="priorityTask spanDatePriorityAssigned">Priority:</span>
-                    <span class="priorityLevel">${task.priority.value}</span><img src="${task.priority.imgSrc}" alt="priority Level">
-                </div>
-                <div class="assignedToSection bodySection"> <!-- auslagern um die einzelnen assigned Contacts anzuzeigen -->
-                    <span class="assignedTo spanDatePriorityAssigned">Assigned To:</span>
-                    <div class="assignedToProfiles">
-                        <div class="taskProfile">
-                            <div class="profileIcon taskProfileIcon" style="background-color:${task.assigned[0].profileColor};">${task.assigned[0].initials}</div>
-                            <span>${task.assigned[0].name}</span>
-                        </div>                    
-                        
+                <h1 class="overlayHeadline">${task.title}</h1>
+                <p class="overlayDescription">
+                    ${task.description}
+                </p>
+                <div class="taskBody">
+                    <div class="dueDateSection bodySection">
+                        <span class="dueDate spanDatePriorityAssigned">Due date:</span>
+                        <span class="date">${task.date}</span>
+                    </div>
+                    <div class="prioritySection bodySection">
+                        <span class="priorityTask spanDatePriorityAssigned">Priority:</span>
+                        <span class="priorityLevel">${task.priority.value}</span><img src="${task.priority.imgSrc}" alt="priority Level">
+                    </div>
+                    <div class="assignedToSection bodySection"> <!-- auslagern um die einzelnen assigned Contacts anzuzeigen -->
+                        <span class="assignedTo spanDatePriorityAssigned">Assigned To:</span>
+                        <div class="assignedToProfiles">
+                            <div class="taskProfile">
+            
+                            </div>
+            
+                        </div>
+                    </div>
+                    <div class="taskSubtask">
+                        ${((task.subtasks == null) ? '' : `<h3 class="subtasksHeadline spanDatePriorityAssigned">Subtasks</h3><div id="loadSubtasks"></div>`)}
+                        ${((task.subtasks == null) ? '' : renderSubtasks(task))}
                     </div>
                 </div>
-                <div class="taskSubtask">
-                    ${((task.subtasks == null) ? '' : `<h3 class="subtasksHeadline spanDatePriorityAssigned">Subtasks</h3><div id="loadSubtasks"></div>`)}
-                    ${((task.subtasks == null) ? '' : renderSubtasks(task))}
-                </div>
-            </div>
-            <div class="showTaskFooter">
-                <div class="deleteEditTask" onclick="deleteTask(${task.id})">
-                    <img class="taskFooterIcon taskFooterIconDelete" src="./img/Mobile/Board/deleteWithText.png">
-                    <!-- <span>Delete</span> -->
-                </div>
-                <img class="barFooter" src="./img/Mobile/Board/bar.png">
-                <div class="deleteEditTask" onclick="openEditTask()">
-                    <img class="taskFooterIcon taskFooterIconEdit" src="./img/Mobile/Board/editTaskWithText.png">
-                    <!-- <span>Edit</span> -->
+                <div class="showTaskFooter">
+                    <div class="deleteEditTask" onclick="deleteTask(${task.id})">
+                        <img class="taskFooterIcon taskFooterIconDelete" src="./img/Mobile/Board/deleteWithText.png">
+                        <!-- <span>Delete</span> -->
+                    </div>
+                    <img class="barFooter" src="./img/Mobile/Board/bar.png">
+                    <div class="deleteEditTask" onclick="openEditTask()">
+                        <img class="taskFooterIcon taskFooterIconEdit" src="./img/Mobile/Board/editTaskWithText.png">
+                        <!-- <span>Edit</span> -->
+                    </div>
                 </div>
             </div>
         </div>
@@ -147,12 +143,12 @@ function viewTask(task) {
 }
 
 function renderSubtasks(subtasks) {
-    for (let i = 0; i < subtasks.Subtasks.length; i++) {
-        console.log(subtasks.Subtasks[i]);
+    for (let i = 0; i < subtasks.subtasks.length; i++) {
+        console.log(subtasks.subtasks[i]);
         return /*html*/ `
         <div class="taskSubtasks">
             <input class="checkboxSubtasks" type="checkbox">
-            <span>${subtasks.Subtasks[i]}</span>
+            <span>${subtasks.subtasks[i]}</span>
         </div>
     `;
     }
