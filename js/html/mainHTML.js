bc9ab29f368a1c7a6ba8012609bc5ea17a2180ef
Tobias Mueller <webstor.game@gmail.com>
2024-07-09 12:42:35 +0200

    Update mainHTML.js

--- js/html/mainHTML.js
+++ js/html/mainHTML.js
@@ -1,6 +1,6 @@
 function headerMobileHtml() {
     return /*html*/ `
-    <img class="headerMobileLogo" src="/img/Mobile/generalElements/joinLogoMobile.png" alt="Join Logo">
+    <img class="headerMobileLogo" src="./img/Mobile/generalElements/joinLogoMobile.png" alt="Join Logo">
     <span class="headerDesktopText">Kanban Project Management Tool</span>
     
     <div class="headerProfileMenu">
@@ -16,19 +16,19 @@ function headerMobileHtml() {
 function menuMobileHtml() {
     return /* html */`
    <a class="menuMobile" href="/summary.html" id="summaryLink">
-       <img src="/img/Mobile/generalElements/summaryIconGray.png" alt="Summary Icon" id="summaryMobileIcon}">
+       <img src="./img/Mobile/generalElements/summaryIconGray.png" alt="Summary Icon" id="summaryMobileIcon}">
        <p>Summary</p>
    </a>   
    <a class="menuMobile" href="/board.html" id="boardLink">
-       <img src="/img/Mobile/generalElements/boardIconGray.png" alt="Board Icon" id="boardMobileIcon">
+       <img src="./img/Mobile/generalElements/boardIconGray.png" alt="Board Icon" id="boardMobileIcon">
        <p>Board</p>
    </a>
    <a class="menuMobile" href="/addTask.html" id="addTaskLink">
-       <img src="/img/Mobile/generalElements/addTaskIconGray.png" alt="Add Task Icon" id="addTaskMobileIcon">
+       <img src="./img/Mobile/generalElements/addTaskIconGray.png" alt="Add Task Icon" id="addTaskMobileIcon">
        <p>Add Task</p>
    </a>
    <a class="menuMobile" href="/contacts.html" id="contactsLink">
-       <img src="/img/Mobile/generalElements/contactsIconGray.png" alt="Contacs Icon" id="contactsMobileIcon">
+       <img src="./img/Mobile/generalElements/contactsIconGray.png" alt="Contacs Icon" id="contactsMobileIcon">
        <p>Contacts</p>
    </a>`;
 };
@@ -37,9 +37,9 @@ function menuMobileHtml() {
 function logoutMobileHtml() {
     return /* html */`
    <div class="logoutContainer slideInRight" id="logout">
-      <a href="/help.html">Help</a>
-      <a href="/legalNotice.html">Legal Notice</a>
-      <a href="/privacyPolice.html">Privacy Policy</a>
+      <a href="./help.html">Help</a>
+      <a href="./legalNotice.html">Legal Notice</a>
+      <a href="./privacyPolice.html">Privacy Policy</a>
       <a href="#" onclick="logout()">Log out</a>
     </div>`;
 };
@@ -61,19 +61,19 @@ function menuDesktopHtml() {            // -- Desktop -- //
         <div class="logoLinks">
             <img src="./img/Desktop/sidebar/joinLogoDesktop.png" alt="Join Logo" class="joinLogoDesktop">
             <div class="quickNavLinks">
-                <a class="menuDesktop" href="/summary.html" id="summaryLinkDesktop">
+                <a class="menuDesktop" href="./summary.html" id="summaryLinkDesktop">
                     <img src="./img/Desktop/sidebar/summaryIconDesktop.png" alt="Summary Icon" id="summaryDesktopIcon" class="iconSidebar">
                     <p>Summary</p>
                 </a>
-                <a class="menuDesktop" href="/addTask.html" id="addTaskLinkDesktop">
+                <a class="menuDesktop" href="./addTask.html" id="addTaskLinkDesktop">
                     <img src="./img/Desktop/sidebar/addTaskIconDesktop.png" alt="Add Task Icon" id="addTaskDesktopIcon" class="iconSidebar">
                     <p>Add Task</p>
                 </a>
-                <a class="menuDesktop" href="/board.html" id="boardLinkDesktop">
+                <a class="menuDesktop" href="./board.html" id="boardLinkDesktop">
                     <img src="./img/Desktop/sidebar/boardIconDesktop.png" alt="Board Icon" id="boardDesktopIcon" class="iconSidebar">
                     <p>Board</p>
                 </a>
-                <a class="menuDesktop" href="/contacts.html" id="contactsLinkDesktop">
+                <a class="menuDesktop" href="./contacts.html" id="contactsLinkDesktop">
                     <img src="./img/Desktop/sidebar/contactsIconDesktop.png" alt="Contacs Icon" id="contactsDesktopIcon" class="iconSidebar">
                     <p>Contacts</p>
                 </a>
