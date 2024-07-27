ce2b8d2867b678a1ef7aed4881f2587f8eaf791b
Tobias Mueller <143468766+Webstor21@users.noreply.github.com>
2024-07-27 22:44:24 +0200

    Update summary.js

--- js/summary.js
+++ js/summary.js
@@ -1,13 +1,33 @@
+/**
+ * Redirects to the board page with a specific section marked.
+ *
+ * This function navigates to the board page (`board.html`) and scrolls to a specific section
+ * identified by the `mark` parameter.
+ *
+ * @param {string} mark - The hash or identifier for the specific section on the board page.
+ */
 function goToBoardUsual(mark) {
     window.location.href = `./board.html#${mark}`;
-};
+}
 
 
+/**
+ * Redirects to the board page.
+ *
+ * This function navigates to the board page (`board.html`).
+ */
 function goToBoard() {
     window.location.href = './board.html';
-};
+}
 
 
+/**
+ * Displays the user's name on the greeting page and transitions to the summary page.
+ *
+ * This asynchronous function fetches the current user's information and displays their name
+ * on the greeting page. If a guest is logged in, the name display is cleared. It then transitions
+ * to the summary page (`summary.html`) after a short delay.
+ */
 async function displayUserName() {
     await getUser();
     let user = users.find(user => user.email && user.email === atob(localStorage.getItem('emailToken')));
@@ -19,9 +39,7 @@ async function displayUserName() {
             document.getElementById('greetingName').innerHTML = '';
         }
         if (user && user.name && guestLoggedIn !== 'true') {
-
             document.getElementById('greetingName').innerHTML = user.name;
-
         } else {
             console.error('User or user name not found');
         }
@@ -30,14 +48,19 @@ async function displayUserName() {
         }, 1000);
 
         setTimeout(() => {
-
             mainContainer.classList.remove('fadeOut');
             window.location.href = "/summary.html";
         }, 2000);
     }
-};
+}
 
 
+/**
+ * Displays a greeting message based on the time of day and guest login status.
+ *
+ * This function sets the greeting message in the `greetingText` element. It adjusts the greeting
+ * for morning, noon, or evening, and checks if a guest is logged in to customize the message.
+ */
 function greeting() {
     let greetingContainer = document.getElementById('greetingText');
     let date = new Date();
@@ -66,81 +89,153 @@ function greeting() {
         }
     }
     greetingContainer.innerHTML = greeting;
-};
+}
 
 
+/**
+ * Counts the number of tasks with the status 'open'.
+ *
+ * @param {Array} tasks - The array of task objects.
+ * @returns {number} The number of open tasks.
+ */
 function countOpenTasks(tasks) {
     return tasks.filter(task => task.status === 'open').length;
-};
+}
 
 
+/**
+ * Counts the number of tasks with the status 'inProgress'.
+ *
+ * @param {Array} tasks - The array of task objects.
+ * @returns {number} The number of in-progress tasks.
+ */
 function countInProgressTasks(tasks) {
     return tasks.filter(task => task.status === 'inProgress').length;
-};
+}
 
 
+/**
+ * Counts the number of tasks with the status 'awaitFeedback'.
+ *
+ * @param {Array} tasks - The array of task objects.
+ * @returns {number} The number of tasks awaiting feedback.
+ */
 function countAwaitFeedbackTasks(tasks) {
     return tasks.filter(task => task.status === 'awaitFeedback').length;
-};
+}
 
 
+/**
+ * Counts the number of tasks with the status 'done'.
+ *
+ * @param {Array} tasks - The array of task objects.
+ * @returns {number} The number of completed tasks.
+ */
 function countDoneTasks(tasks) {
     return tasks.filter(task => task.status === 'done').length;
-};
+}
 
 
+/**
+ * Counts the number of tasks with the priority 'Urgent'.
+ *
+ * @param {Array} tasks - The array of task objects.
+ * @returns {number} The number of urgent tasks.
+ */
 function countUrgentTasks(tasks) {
     return tasks.filter(task => task.priority.value === 'Urgent').length;
-};
+}
 
 
+/**
+ * Counts the total number of tasks.
+ *
+ * @param {Array} tasks - The array of task objects.
+ * @returns {number} The total number of tasks.
+ */
 function countTasks(tasks) {
     return tasks.length;
-};
+}
 
 
+/**
+ * Displays the count of open tasks.
+ *
+ * This asynchronous function fetches the tasks and updates the UI with the count of open tasks.
+ */
 async function displayCountToDo() {
     let tasks = await getTask();
     let openTasksCount = countOpenTasks(tasks);
     document.getElementById('openTasks').innerHTML = openTasksCount;
-};
+}
 
 
+/**
+ * Displays the count of in-progress tasks.
+ *
+ * This asynchronous function fetches the tasks and updates the UI with the count of in-progress tasks.
+ */
 async function displayCountInProgress() {
     let tasks = await getTask();
     let inProgressTasksCount = countInProgressTasks(tasks);
     document.getElementById('inProgressTasks').innerHTML = inProgressTasksCount;
-};
+}
 
 
+/**
+ * Displays the count of tasks awaiting feedback.
+ *
+ * This asynchronous function fetches the tasks and updates the UI with the count of tasks awaiting feedback.
+ */
 async function displayCountAwaitFeedback() {
     let tasks = await getTask();
     let awaitFeedbackTasksCount = countAwaitFeedbackTasks(tasks);
     document.getElementById('awaitFeedbackTasks').innerHTML = awaitFeedbackTasksCount;
-};
+}
 
 
+/**
+ * Displays the count of completed tasks.
+ *
+ * This asynchronous function fetches the tasks and updates the UI with the count of completed tasks.
+ */
 async function displayCountDone() {
     let tasks = await getTask();
     let doneTasksCount = countDoneTasks(tasks);
     document.getElementById('doneTasks').innerHTML = doneTasksCount;
-};
+}
 
 
+/**
+ * Displays the count of urgent tasks.
+ *
+ * This asynchronous function fetches the tasks and updates the UI with the count of urgent tasks.
+ */
 async function displayCountUrgent() {
     let tasks = await getTask();
     let urgentTasksCount = countUrgentTasks(tasks);
     document.getElementById('urgentTasks').innerHTML = urgentTasksCount;
-};
+}
 
 
+/**
+ * Displays the total count of tasks.
+ *
+ * This asynchronous function fetches the tasks and updates the UI with the total count of tasks.
+ */
 async function displayCountAllTasks() {
     let tasks = await getTask();
     let allTasksCount = countTasks(tasks);
     document.getElementById('allTasks').innerHTML = allTasksCount;
-};
+}
 
 
+/**
+ * Displays the earliest deadline date for urgent tasks.
+ *
+ * This asynchronous function fetches the tasks, finds the earliest deadline among urgent tasks,
+ * and displays the date in the UI.
+ */
 async function displayDeadline() {
     let tasks = await getTask();
     let deadlineDate = document.getElementById('deadlineDate');
@@ -154,9 +249,14 @@ async function displayDeadline() {
     } else {
         deadlineDate.innerHTML = earliestDeadline.toLocaleDateString('en-EN', { month: 'long', day: 'numeric', year: 'numeric' });
     }
-};
+}
 
 
+/**
+ * Initializes the task count displays.
+ *
+ * This function triggers the display functions for various task counts, updating the UI accordingly.
+ */
 function countInit() {
     displayCountToDo();
     displayCountInProgress();
@@ -165,7 +265,7 @@ function countInit() {
     displayCountUrgent();
     displayCountAllTasks();
     displayDeadline();
-};
+}
 
 
 function countOpenTasks(tasks) {
@@ -198,6 +298,12 @@ function countTasks(tasks) {
 };
 
 
+/**
+ * Displays a greeting message on the summary page.
+ *
+ * This asynchronous function fetches the user data, determines the appropriate greeting based on the time of day,
+ * and displays it along with the user's name on the summary page.
+ */
 async function greetingSummary() {
     await getUser();
     let guestLoggedIn = localStorage.getItem('guestLoggedIn');
@@ -239,4 +345,4 @@ async function greetingSummary() {
           <div class="greetingText" id="greetingText">${greeting}</div>
           <div class="greetingText" id="greetingName">${username}</div>
         </div>`;
-};
+}
