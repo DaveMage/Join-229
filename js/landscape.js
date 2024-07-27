a5cb4d812e9a6b535a103dd39cf16cc50d6e1acd
Tobias Mueller <143468766+Webstor21@users.noreply.github.com>
2024-07-27 22:31:17 +0200

    Update landscape.js

--- js/landscape.js
+++ js/landscape.js
@@ -1,15 +1,23 @@
+/**
+ * Handles changes in screen orientation and adjusts the UI accordingly.
+ *
+ * This function checks the screen orientation and displays a warning if the device is
+ * in landscape mode. It hides the warning and displays the normal UI if the device is in portrait mode.
+ */
 function handleOrientationChange() {
     if (window.orientation === 90 || window.orientation === -90) {
+        // Device is in landscape mode
         document.querySelector('.landscape-warning').style.display = 'block';
         document.querySelector('.noLandscapeMode').style.display = 'none';
     } else {
+        // Device is in portrait mode
         document.querySelector('.landscape-warning').style.display = 'none';
         document.querySelector('.noLandscapeMode').style.display = 'block';
     }
-};
-
+}
 
 window.addEventListener('orientationchange', handleOrientationChange);
+
 
 document.addEventListener('DOMContentLoaded', function () {
     handleOrientationChange();
