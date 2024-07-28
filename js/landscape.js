b340638e7c013a6dfe697ea14391bf55415493b0
DaveMage <152330450+DaveMage@users.noreply.github.com>
2024-07-28 22:47:06 +0200

    Entwicklerkommentare entfernt landscape js

--- js/landscape.js
+++ js/landscape.js
@@ -6,25 +6,19 @@
  */
 function handleOrientationChange() {
     const orientation = screen.orientation || screen.mozOrientation || screen.msOrientation;
-
-    // Select elements
     const landscapeWarning = document.querySelector('.landscape-warning');
     const noLandscapeMode = document.querySelector('.noLandscapeMode');
 
-    // Ensure elements exist before accessing their styles
     if (landscapeWarning && noLandscapeMode) {
         if (orientation.type.includes('landscape')) {
-            // Device is in landscape mode
             landscapeWarning.style.display = 'block';
             noLandscapeMode.style.display = 'none';
         } else if (orientation.type.includes('portrait')) {
-            // Device is in portrait mode
             landscapeWarning.style.display = 'none';
             noLandscapeMode.style.display = 'block';
         }
     }
 }
 
-// Listen for orientation changes
 window.addEventListener('orientationchange', handleOrientationChange);
-handleOrientationChange(); // Call the function initially to set the correct display
+handleOrientationChange(); 
